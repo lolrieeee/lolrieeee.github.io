7f7f6c492ac01ae70121a6f834d05565cd86bf54
Larry Bui <lolrieeee>
2017-09-06 19:29:39 -0700

    new

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$('.smooth-scroll').on('click', function(e){
+    e.preventDefault();
+    console.log('hi');
+
+    var sectionId = '#' + $(this).data('section');
+    console.log(sectionId);
+    var sectionHeight = $(sectionId).height();
+    console.log(sectionHeight);
+
+    $('body').height(sectionHeight);
+
+    var sectionPosition = $(sectionId).offset().left;
+    console.log(sectionPosition);
+
+    $('html, body').animate({scrollLeft:sectionPosition},800);
+});
+
+$('.project-clicked').on('click', function(){
+    console.log('clicked');
+    $(this).siblings('.portfolio-content').height('100vh');
+    $(this).siblings('.portfolio-content').css('padding-top', '120px');
+    $(this).siblings('.portfolio-content').css('padding-bottom', '120px');
+});
