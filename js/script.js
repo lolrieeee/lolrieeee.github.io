7bb6aec527597e72b49c6f8c937f6fc0ec93bd0d
Larry Bui <lolrieeee>
2017-09-09 02:34:40 -0700

    new animations

--- js/script.js
+++ js/script.js
@@ -10,7 +10,9 @@ function throttle(func,interval) {
 }
 
 var currentNav = 0;
+var currentPortfolio = 0;
 var navIndicatorPos = $('.smooth-scroll:eq(0)').position().left;
+var navIndicatorColor = ['#2accff','#b322c4','#14bf6d','#ffa53b','#ff4a9d','#633cff'];
 
 $('.smooth-scroll').on('click', function(e){
     e.preventDefault();
@@ -27,12 +29,18 @@ $('.smooth-scroll').on('click', throttle(function(){
     $(sectionId).scrollTop('0');
     currentNav = $(this).index();
     navIndicatorPos = $('.smooth-scroll:eq(' + currentNav + ')').position().left;
+    $('#nav-indicator').css('width','5px');
+    $('#nav-indicator').css('background-color', navIndicatorColor[currentNav]);
+    setTimeout(function(){
+        $('#nav-indicator').css('width','30px');
+    },100);
     $('#nav-indicator').css('left', navIndicatorPos);
     $('#site-container').stop().animate({scrollLeft:sectionPosition * currentNav},800);
     },900));
 
 $('.project-clicked').on('click', function(){
-    console.log('open');
+    currentPortfolio = $(this).parent().index() - 1;
+    console.log(currentPortfolio);
     $(this).siblings('.portfolio-content').addClass('open');
     setTimeout(function(){
         $('.backnav-container').addClass('open');
@@ -41,9 +49,32 @@ $('.project-clicked').on('click', function(){
 });
 
 $('.close').on('click', function(){
-    console.log('close');
     $('.open').removeClass('open');
     $('.backnav-container').removeClass('open');
+});
+
+$('.previous').on('click', function(){
+    currentPortfolio = currentPortfolio - 1;
+    if ( currentPortfolio < 0 ) {
+        currentPortfolio = document.getElementsByClassName('portfolioset').length - 1;
+    }
+    setTimeout(function(){
+        $('.portfolio-content.open').scrollTop('0');
+    },300);
+    $('.portfolio-content.open').removeClass('open');
+    $('.portfolio-content:eq(' + currentPortfolio + ')').addClass('open');
+});
+
+$('.next').on('click', function(){
+    currentPortfolio = currentPortfolio + 1;
+    if ( currentPortfolio > document.getElementsByClassName('portfolioset').length - 1 ) {
+        currentPortfolio = 0;
+    }
+    setTimeout(function(){
+        $('.portfolio-content.open').scrollTop('0');
+    },300);
+    $('.portfolio-content.open').removeClass('open');
+    $('.portfolio-content:eq(' + currentPortfolio + ')').addClass('open');
 });
 
 $(window).on('resize', function(){
